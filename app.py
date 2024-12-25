40927ae412bc44855dc001e776612bbca45a8661
Smrutirekha Swain <145380039+smruti8391@users.noreply.github.com>
2024-12-25 19:12:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 #import the model
 pipe = pickle.load(open('pipe.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
-st.title("Laptop Price Predictor")
+st.title("Laptop Price Predictor 💻")
 
 #brand
 company = st.selectbox('Brand',df['Company'].unique())
